987670889d3264221fbb88ba13c019915157bea2
Francisco Hauva <phauva@proton.me>
2024-03-24 09:34:51 -0300

    refactor: extract color types and translations to another file, improve pokemon type chip styling

--- src/components/PokemonTypeChip.tsx
+++ src/components/PokemonTypeChip.tsx
@@ -1,9 +1,18 @@
-export default function PokemonTypeChip({ typeName }: { typeName: string }) {
-	const type = types[typeName] || types.default;
+import { pokemonTypeColors, pokemonTypeTranslations } from "@/lib/pokemon";
+
+export default function PokemonTypeChip({
+	size,
+	typeName,
+}: {
+	size?: Size;
+	typeName: string;
+}) {
+	const type = pokemonTypeColors[typeName] || pokemonTypeColors.default;
+	const typeText = pokemonTypeTranslations[typeName] || typeName;
 
 	return (
 		<span
-			className="px-4 h-min rounded-[3px] text-[11px] font-medium text-center"
+			className={`${sizeToStyle[size || "sm"]} h-min font-medium text-center `}
 			style={{
 				background:
 					"linear-gradient(180deg, " +
@@ -14,114 +23,17 @@ export default function PokemonTypeChip({ typeName }: { typeName: string }) {
 				color: type.textColor,
 			}}
 		>
-			{type.translation || typeName}
+			{typeText}
 		</span>
 	);
 }
 
-const types: {
-	[key: string]: {
-		primaryColor: string;
-		secondaryColor?: string;
-		textColor: string;
-		translation?: string;
-	};
-} = {
-	default: {
-		primaryColor: "#A4ACAF",
-		textColor: "#000",
-		translation: "Normal",
-	},
-	bug: {
-		primaryColor: "#729F3F",
-		textColor: "#fff",
-		translation: "Bicho",
-	},
-	dark: {
-		primaryColor: "#705848",
-		textColor: "#fff",
-		translation: "Siniestro",
-	},
-	dragon: {
-		primaryColor: "#7038F8",
-		textColor: "#fff",
-		translation: "Dragón",
-	},
-	electric: {
-		primaryColor: "#EED535",
-		textColor: "#000",
-		translation: "Eléctrico",
-	},
-	fairy: {
-		primaryColor: "#EE99AC",
-		textColor: "#000",
-		translation: "Hada",
-	},
-	fighting: {
-		primaryColor: "#D56723",
-		textColor: "#fff",
-		translation: "Lucha",
-	},
-	fire: {
-		primaryColor: "#FD7D24",
-		textColor: "#fff",
-		translation: "Fuego",
-	},
-	flying: {
-		primaryColor: "#3DC7EF",
-		secondaryColor: "#BDB9B8",
-		textColor: "#000",
-		translation: "Volador",
-	},
-	ghost: {
-		primaryColor: "#705898",
-		textColor: "#fff",
-		translation: "Fantasma",
-	},
-	grass: {
-		primaryColor: "#78C850",
-		textColor: "#000",
-		translation: "Planta",
-	},
-	ground: {
-		primaryColor: "#F7DE3F",
-		secondaryColor: "#AB9842",
-		textColor: "#000",
-		translation: "Tierra",
-	},
-	ice: {
-		primaryColor: "#51C4E7",
-		textColor: "#000",
-		translation: "Hielo",
-	},
-	normal: {
-		primaryColor: "#A4ACAF",
-		textColor: "#000",
-		translation: "Normal",
-	},
-	poison: {
-		primaryColor: "#B97FC9",
-		textColor: "#fff",
-		translation: "Veneno",
-	},
-	psychic: {
-		primaryColor: "#F366B9",
-		textColor: "#fff",
-		translation: "Psíquico",
-	},
-	rock: {
-		primaryColor: "#A38C21",
-		textColor: "#fff",
-		translation: "Roca",
-	},
-	steel: {
-		primaryColor: "#9EB7B8",
-		textColor: "#000",
-		translation: "Acero",
-	},
-	water: {
-		primaryColor: "#4592C4",
-		textColor: "#fff",
-		translation: "Agua",
-	},
-};
+const sizeToStyle = {
+	sm: "px-4 py-[0.4px] text-[11px] rounded-[3px]",
+	md: "px-5 py-[1px] text-[12px] rounded-md",
+	lg: "px-7 py-[2px] text-[14px] rounded-md",
+	xl: "px-8 py-[4px] text-[15px] rounded-md",
+	"2xl": "px-10 py-[6px] text-[16px] rounded-md",
+} as const;
+
+type Size = keyof typeof sizeToStyle;
--- src/lib/pokemon.ts
+++ src/lib/pokemon.ts
@@ -56,3 +56,114 @@ export async function getPokemonList(
 
 	return data;
 }
+
+export const pokemonTypeColors: {
+	[key: string]: {
+		primaryColor: string;
+		secondaryColor?: string;
+		textColor: string;
+	};
+} = {
+	default: {
+		primaryColor: "#A4ACAF",
+		textColor: "#212121",
+	},
+	bug: {
+		primaryColor: "#729F3F",
+		textColor: "#fff",
+	},
+	dark: {
+		primaryColor: "#705848",
+		textColor: "#fff",
+	},
+	dragon: {
+		primaryColor: "#53A4CF",
+		secondaryColor: "#F16E57",
+		textColor: "#fff",
+	},
+	electric: {
+		primaryColor: "#EED535",
+		textColor: "#212121",
+	},
+	fairy: {
+		primaryColor: "#EE99AC",
+		textColor: "#212121",
+	},
+	fighting: {
+		primaryColor: "#D56723",
+		textColor: "#fff",
+	},
+	fire: {
+		primaryColor: "#FD7D24",
+		textColor: "#fff",
+	},
+	flying: {
+		primaryColor: "#3DC7EF",
+		secondaryColor: "#BDB9B8",
+		textColor: "#212121",
+	},
+	ghost: {
+		primaryColor: "#705898",
+		textColor: "#fff",
+	},
+	grass: {
+		primaryColor: "#9BCC50",
+		textColor: "#212121",
+	},
+	ground: {
+		primaryColor: "#F7DE3F",
+		secondaryColor: "#AB9842",
+		textColor: "#212121",
+	},
+	ice: {
+		primaryColor: "#51C4E7",
+		textColor: "#212121",
+	},
+	normal: {
+		primaryColor: "#A4ACAF",
+		textColor: "#212121",
+	},
+	poison: {
+		primaryColor: "#B97FC9",
+		textColor: "#fff",
+	},
+	psychic: {
+		primaryColor: "#F366B9",
+		textColor: "#fff",
+	},
+	rock: {
+		primaryColor: "#A38C21",
+		textColor: "#fff",
+	},
+	steel: {
+		primaryColor: "#9EB7B8",
+		textColor: "#212121",
+	},
+	water: {
+		primaryColor: "#4592C4",
+		textColor: "#fff",
+	},
+};
+
+export const pokemonTypeTranslations: {
+	[key: string]: string;
+} = {
+	bug: "Bicho",
+	dark: "Siniestro",
+	dragon: "Dragón",
+	electric: "Eléctrico",
+	fairy: "Hada",
+	fighting: "Lucha",
+	fire: "Fuego",
+	flying: "Volador",
+	ghost: "Fantasma",
+	grass: "Planta",
+	ground: "Tierra",
+	ice: "Hielo",
+	normal: "Normal",
+	poison: "Veneno",
+	psychic: "Psíquico",
+	rock: "Roca",
+	steel: "Acero",
+	water: "Agua",
+};
