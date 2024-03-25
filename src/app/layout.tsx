84de83110cceac691972a9bce5444fbbee044469
Francisco Hauva <phauva@proton.me>
2024-03-25 15:11:48 -0300

    refactor: remove import for Inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import bg from "@/assets/bg.png";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 
