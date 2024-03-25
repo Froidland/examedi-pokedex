a11cc9d7ce030c4831af579b17546257d86deb78
Francisco Hauva <phauva@proton.me>
2024-03-25 15:08:17 -0300

    fix: react shenanigans

--- src/components/icons/FemaleSignIcon.tsx
+++ src/components/icons/FemaleSignIcon.tsx
@@ -12,9 +12,9 @@ export function FemaleSignIcon(props: SVGProps<SVGSVGElement>) {
 			<path
 				fill="none"
 				stroke="#000"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="1.5"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth="1.5"
 				d="M12 15a6 6 0 1 0 0-12a6 6 0 0 0 0 12m0 0v6m-2-2h4"
 			></path>
 		</svg>
--- src/components/icons/MaleSignIcon.tsx
+++ src/components/icons/MaleSignIcon.tsx
@@ -12,9 +12,9 @@ export function MaleSignIcon(props: SVGProps<SVGSVGElement>) {
 			<path
 				fill="none"
 				stroke="#000"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="1.5"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth="1.5"
 				d="M14.232 9.747a6 6 0 1 0-8.465 8.506a6 6 0 0 0 8.465-8.506m0 0L20 4m0 0h-4m4 0v4"
 			></path>
 		</svg>
