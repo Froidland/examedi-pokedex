8f43ab7a0f8f8de7112d74279f6e9739b43a990c
Francisco Hauva <phauva@proton.me>
2024-03-25 17:31:46 -0300

    feat: get id from the list url string

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,11 +1,26 @@
 import type { NamedAPIResource } from "@/lib/types";
 import PokemonCard from "./PokemonCard";
 
+const pokemonIdRegex = /https:\/\/pokeapi.co\/api\/v2\/pokemon\/(\d+)\//;
+
+function getPokemonIdFromUrl(url: string) {
+	const match = pokemonIdRegex.exec(url);
+	if (match) {
+		return +match[1];
+	}
+
+	return null;
+}
+
 export default function PokemonList({ data }: { data: NamedAPIResource[] }) {
 	return (
 		<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
 			{data.map((pokemon, index) => (
-				<PokemonCard key={index} name={pokemon.name} pokemonId={index + 1} />
+				<PokemonCard
+					key={index}
+					name={pokemon.name}
+					pokemonId={getPokemonIdFromUrl(pokemon.url) || index}
+				/>
 			))}
 		</div>
 	);
